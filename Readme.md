858995f696b37fdd2ef4927fd462762d08210dd7
Saulo Aguiar <sauloaguiar89@gmail.com>
2016-03-09 22:19:11 -0600

    Adding bash script and Readme file

---
+++ Readme.md
@@ -0,0 +1,20 @@
+# Test Data Collector
+
+## Objective
+
+  This project aims to collect test data of open source repos.
+  Right now, the focus is to track Scala based projects that can be covered using [Scoverage](https://github.com/scoverage/sbt-scoverage).
+
+
+## Projects Being Tracked
+
+  This initial list was compiled using [CodeTriage](http://www.codetriage.com/) as the main source.
+
+| Project               |            Build ok?             | Scoverage?  |
+| :----------------------------------------------------------: |:--:|:--:|
+| [Scalariform](https://github.com/scala-ide/scalariform)      | ok | ok |
+| [GitBucket](https://github.com/gitbucket/gitbucket)          | ok | ok |
+| [Breeze](https://github.com/scalanlp/breeze)                 | ok | ok |
+| [Cats](https://github.com/typelevel/cats)                    | ok | no |
+| [Eventuate](https://github.com/RBMHTechnology/eventuate)     | ok | no |
+| [Algebird](https://github.com/twitter/algebird)              | ok | no |
---
+++ bash/test.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Usage
+# Call this script specifying three arguments
+# 1. a folder name to hold test data
+# 2. the path to the repo
+
+args=("$@")
+echo ${args[0]}
+echo ${args[1]}
+cd ${args[1]}
+#$(git checkout master)
+base_dir="/Users/sauloaguiar/Desktop"
+git_history=($(git log --pretty=format:"%H"))
+git_history_length=${#git_history[@]}
+for ((git_index=0; git_index<${git_history_length}; git_index++));
+  do
+    # fresh the repo to its initial state
+    git pull origin master
+    git checkout master
+
+    echo ${git_history[git_index]}
+    # create dir to hold test data
+    mkdir -p $base_dir/${args[0]}/${git_history[git_index]}
+
+    # checkout to the sha1
+    git checkout ${git_history[git_index]}
+
+    # find where plugins.sbt is
+    plugins_sbt_path=($(find . -name plugins.sbt))
+    echo ${plugins_sbt_path}
+    # append the scoverage lib to the file
+    echo 'addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.3.5")' >> $plugins_sbt_path
+
+    # run the test suite
+    sbt clean coverage test -mem 4096
+
+    # run scoverage report
+    sbt coverageReport
+
+    # find xml generated data
+    scoverage_path=($(find . -name "scoverage-report"))
+    echo ${scoverage_path}
+
+    scoverage_path+="/scoverage.xml"
+    # copy scoverage.xml file
+    cp ${scoverage_path} $base_dir/${args[0]}/${git_history[git_index]}/
+
+    git checkout -- .
+done
